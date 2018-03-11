1c92f73a5d2658bdb9ccd62076f4722bce5532ee
mixxxxx <spartacusxxxxxx@gmail.com>
2018-03-11 21:40:26 +0300

    zestimate

--- scrapy_class.py
+++ scrapy_class.py
@@ -3,6 +3,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 
 #from selenium.common.exceptions import TimeoutException
 
@@ -10,30 +11,67 @@ from selenium.webdriver.chrome.options import Options
 class ZillowSpider:
 	def __init__(self, links):
 		self.links = links
-		self.json = None
   
 
 
-	def get_one_request(self, url):
-
-		#browser = webdriver.Chrome('/usr/local/bin/chromedriver')
+	def init_driver(file_path):
 		options = Options()
 		#options.add_experimental_option("excludeSwitches",["ignore-certificate-errors"])
-		#options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
 		#options.add_argument("--start-maximized")
 		#options.add_argument("allow-running-insecure-content")
 		#options.add_argument("test-type")
-		options.add_argument("--headless")
-		#options.add_argument("--window-size=1920x1080")
+
+		#options.add_argument("--headless")
+
 		#options.add_argument("--disable-gpu") 
 
 		browser = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=options)	
-		browser.get('https://yandex.ru')
+
+		browser.wait = WebDriverWait(browser, 10)
+
+		return(browser)
+
+	def _is_empty(self, obj):
+		if any([len(obj) == 0, obj == "null"]):
+			return(True)
+		else:
+			return(False)
+
+
+
+	def get_zestimate(self, soup_obj):
+		#with open("file.txt", "w") as file:
+			#print(soup_obj, file=file)
+		#print(soup_obj)
+		zestimate = soup_obj.find("div", {"class" : "zestimate-value"})#.get_text().strip()
+		print(zestimate)
+		try:
+			zestimate = soup_obj.find("div", {"class" : "main-row  home-summary-row"}).get_text().strip()
+			print(zestimate)
+			zestimate = soup_obj.find("div", class_="zestimate-value")#.get_text().strip()
+			print(zestimate)
+			zestimate = soup_obj.findAll("div", {"class": "zestimate-value"})
+			print(zestimate)
+		except (ValueError, AttributeError):
+			zestimate = "NA"
+		if self._is_empty(zestimate):
+			zestimate = "NA"
+		return(zestimate)
+
+
+
+	def get_one_request(self, browser, url):
+
+		browser.get(url)
 
 		html = browser.page_source
 		soup = BeautifulSoup(html, "lxml")
-		for tag in soup.find_all('title'):
-			print(tag.text)
+		#print(soup)
+		#for tag in soup.find_all('title'):
+			#print(tag.text)
+
+		zestimate = self.get_zestimate(soup)
+		print(zestimate)
 
 
-		browser.quit()
+		#browser.quit()
--- zillow_scrap.py
+++ zillow_scrap.py
@@ -1,5 +1,8 @@
 import csv
 import scrapy_class
+from bs4 import BeautifulSoup
+#from lxml import etree
+
 
 def csv_reader(file_obj):
 	links = []
@@ -28,17 +31,66 @@ if __name__ == "__main__":
 		links = csv_reader(f_obj)
 
 
+	# with open('file.html', 'r') as file:
+	# 	#ssoup = BeautifulSoup(file.read(), 'html.parser')
+	# 	ssoup = BeautifulSoup(file.read(), "lxml")
+		#print(ssoup)
+	#zestimate = ssoup.find("span", {"itemprop" : "streetAddress"}).get_text().strip()
+	#zestimate = ssoup.find("div", {"class": "yui3-widget yui3-lightbox yui3-widget-positioned yui3-widget-stacked yui3-lightbox-modal yui3-lightbox-focused"})#.get_text().strip()
+
+	#zestimate = ssoup.find("div", {"class": "yui3-widget yui3-lightbox yui3-widget-positioned yui3-widget-stacked yui3-lightbox-modal yui3-lightbox-focused"})
+	
+	#zestimate = zestimate.find("div", {"class": "dialog yui3-lightbox-content"})
+
+	#zestimate = zestimate.find("div", {"class": "yui3-widget-bd lightbox-body zsg-tooltip-viewport"})
+
+
+	#zestimate = ssoup.find("div", {"class": "zsg-tooltip-viewport"})
+	
+	#zestimate = ssoup.find("div", {"id": "home-details-module-zone"})
+
+	#zestimate = ssoup.find(id="homeValue")
+
+	# <section class="home-details-collapsible-component-CollapsibleContainer collapsed collapsible" id="homeValue">
+	# 	<h2 class="zsg-h2 hdp-collapsible-title">Home Value</h2>
+	# 	<div class="collapsible-content"></div>
+	# </section>
+
+
+
+
+	#zestimate = ssoup.find("div", {"class" : "zestimate-value"})#.get_text().strip()
+	#print(zestimate.prettify())
+
+
+
+
 	ZillowSpider = scrapy_class.ZillowSpider(links)
 
-	print(links[0])
+	browser = ZillowSpider.init_driver()
 
-	ZillowSpider.get_one_request(links[0])
-	#ZillowSpider.parse(links)
+	#browser.implicitly_wait(15)
+
+	browser.get(links[0])
+
+	elm = browser.find_element_by_id('homeValue')
+	browser.implicitly_wait(2)
+	elm.click()
+
+	element_text = elm.text
+	print(element_text)
+	print(type(element_text))
+	print(element_text.split())
+	print(element_text.split()[3])
+	print(element_text.split()[6] + element_text.split()[7] + element_text.split()[8])
 
 
 
-	#print(ZillowSpider.name)
-	#print(ZillowSpider.start_urls)
+	#ZillowSpider.get_one_request(browser, links[0] )
+	
 
-	#print(links)
+
+
+
+
 
