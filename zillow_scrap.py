d258319b5a89f0d302f867d50298b05abcadc7d6
mixxxxx <spartacusxxxxxx@gmail.com>
2018-03-14 22:44:10 +0300

    all works with add fields

--- zillow_scrap.py
+++ zillow_scrap.py
@@ -17,24 +17,26 @@ def update_csv_cell(ZillowSpider, browser):
 
 		for current_line in reader:
 			#print(current_line)
-			# i+=1
-			# if i == 4:
-			# 	break
+			i+=1
+			if i == 5:
+				break
 
 			if current_line[10] == 'url':
-				print(current_line)
-				current_line.append('zestimate')
-				current_line.append('zRange')
-				current_line.append('builtIn')
-				current_line.append('builtBy')
-				current_line.append('comName')
-				current_line.append('parking')
-				current_line.append('school')
-				current_line.append('parking_details')
-				current_line.append('lastSold')
-				lines.append(current_line)
+				pass
+				# print(current_line)
+				# current_line.append('zestimate')
+				# current_line.append('zRange')
+				# current_line.append('builtIn')
+				# current_line.append('builtBy')
+				# current_line.append('comName')
+				# current_line.append('parking')
+				# current_line.append('school')
+				# current_line.append('parking_details')
+				# current_line.append('lastSold')
+				# current_line.append('priceSqft')
+				# lines.append(current_line)
 			else:
-				zestimate , zRange, builtIn, builtBy, comName, parking, school, parking_details, lastSold= ZillowSpider.get_one_request(browser, current_line[10])
+				zestimate , zRange, builtIn, builtBy, comName, parking, school, parking_details, lastSold, priceSqft = ZillowSpider.get_one_request(browser, current_line[10])
 				print(current_line[10])
 				current_line.append(zestimate)
 				current_line.append(zRange)
@@ -45,6 +47,7 @@ def update_csv_cell(ZillowSpider, browser):
 				current_line.append(school)
 				current_line.append(parking_details)
 				current_line.append(lastSold)
+				current_line.append(priceSqft)
 				lines.append(current_line)
 
 
@@ -52,7 +55,7 @@ def update_csv_cell(ZillowSpider, browser):
 	file_name = "%s_%s.csv" % (str(time.strftime("%Y-%m-%d")),
                            str(time.strftime("%H%M%S")))
 	columns = ["address", "city", "state", "zip", "price", "sqft", "bedrooms",
-           "bathrooms", "days_on_zillow", "sale_type", "url", "zestimate", "zRange", "builtIn", "builtBy", "comName", "parking", "school", "parking_details", "lastSold"]
+           "bathrooms", "days_on_zillow", "sale_type", "url", "zestimate", "zRange", "builtIn", "builtBy", "comName", "parking", "school", "parking_details", "lastSold", "priceSqft"]
 	pd.DataFrame(lines, columns = columns).to_csv(file_name, index = False)
 
 
